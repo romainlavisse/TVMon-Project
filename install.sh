947d1f5ff19268c4511ea0ea0a4694f0479a6a59
romainlavisse <romain.gourrion@orange.fr>
2020-03-04 15:36:01 +0100

    add "wireshark-commun" in script

--- install.sh
+++ install.sh
@@ -8,6 +8,7 @@ sudo apt-get install vlc
 sudo apt-get install wmctrl
 sudo apt install python3-pip
 sudo apt install python3-tk
+sudo apt install wireshark-commun
 
 ##### pip3 install
 pip3 install pathlib
